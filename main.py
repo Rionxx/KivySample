842fa45e1708d0b86ae35fe40af55c3ff28303d9
Rion <rionfx.42@gmail.com>
2021-10-28 11:30:17 +0900

    Could Display Text by 'Hello kivy'.

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 from kivy.app import App
+from kivy.core import text
+from kivy.uix.label import Label
 
-App().run()
+from kivy.core.text import LabelBase, DEFAULT_FONT
+from kivy.resources import resource_add_path
+
+#Windows is C:\Windows\Fonts
+#Mac is /System/Library/Fonts
+
+resource_add_path('/System/Library/Fonts')
+
+class TextApp(App):
+  def build(self):
+      return Label(text='Hello Kivy')
+
+TextApp().run()
